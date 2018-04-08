3485e60497d7d3c7987cf87551469d1529a6806b
Edward Timmer <edtimmer@gmail.com>
2018-04-08 11:44:02 -0400

    added message when there are no students or campuses

--- src/Campuses.js
+++ src/Campuses.js
@@ -3,24 +3,35 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const Campuses = ({campuses})=> {
-  return (
-    <div>
-      <h2>All Campuses</h2>
-      <p><i>Number of Campuses:</i> <strong>{campuses.length}</strong></p>
-      <Link to={'/campuses/create'}>Add Campus</Link>
-      <ul>
-        {
-          campuses.map(campus => {
-            return (
-              <li key={campus.id}>
-                <Link to={`/campuses/${campus.id}`}>{campus.name}</Link>
-              </li>
-            )
-          })
-        }
-      </ul>
-    </div>
-  )  
+  if (campuses.length > 0) {
+    return (
+      <div>
+        <h2>All Campuses</h2>
+        <p><i>Number of Campuses:</i> <strong>{campuses.length}</strong></p>
+        <Link to={'/campuses/create'}>Add Campus</Link>
+        <ul>
+          {
+            campuses.map(campus => {
+              return (
+                <li key={campus.id}>
+                  <Link to={`/campuses/${campus.id}`}>{campus.name}</Link>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    )  
+  }
+  else {
+    return (
+      <div>
+        <h4><i>There are no campuses at present.  Please add a campus.</i></h4>
+        <Link to={'/campuses/create'}>Add Campus</Link>
+      </div>
+    )
+  }
+
 }
 
 const mapStateToProps = ({ campuses })=> {
--- src/Students.js
+++ src/Students.js
@@ -4,35 +4,44 @@ import { Link } from 'react-router-dom';
 import StudentSelectCampus from './StudentSelectCampus'
 
 const Students = ({students, campuses})=> {
-  return (
-    <div>
-      <h2>All Students</h2>
-      <p><i>Number of all students:</i> <strong>{students.length}</strong></p>
-      <Link to={'/students/create'}>Add Student</Link>
-      <ul>
-        {
-          students.map(student => {
-            const campus = campuses.find(campus=> campus.id === student.campusId);
-            console.log('campus is:', campus)
-            const campusName = campus ? <Link to={`/campuses/${campus.id}`}>{campus.name}
-        </Link> : 'none'
-  
-            return (
-              <li key={student.id}>
-                <Link to={`/students/${student.id}`}>{student.name}</Link>
-                <br />
-                Enrolled in:
-                {campusName}
-                <br />               
-              </li>  
-         
-            )
-          })          
-        }
-      </ul>
-      
-    </div>
-  )  
+  if (students.length > 0) {
+    return (
+      <div>
+        <h2>All Students</h2>
+        <p><i>Number of all students:</i> <strong>{students.length}</strong></p>
+        <Link to={'/students/create'}>Add Student</Link>
+        <ul>
+          {
+            students.map(student => {
+              const campus = campuses.find(campus=> campus.id === student.campusId);
+              console.log('campus is:', campus)
+              const campusName = campus ? <Link to={`/campuses/${campus.id}`}>{campus.name}
+          </Link> : 'none'
+    
+              return (
+                <li key={student.id}>
+                  <Link to={`/students/${student.id}`}>{student.name}</Link>
+                  <br />
+                  Enrolled in:
+                  {campusName}
+                  <br />               
+                </li>  
+           
+              )
+            })          
+          }
+        </ul>      
+      </div>
+    ) 
+  }
+  else {
+    return (
+      <div>
+        <h4><i>There are no students.  Please add a student.</i></h4>
+        <Link to={'/students/create'}>Add Student</Link>
+      </div>
+    )
+  } 
 }
 
 const mapStateToProps = ({ students, campuses })=> {
